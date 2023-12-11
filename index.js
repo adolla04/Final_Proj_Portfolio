19086cbe22ef1b1113eebf3e843e32c4ea0baf06
Zoe Rasico <MY_NAME@example.com>
2023-12-11 06:59:19 -0500

    many adjustments

--- index.js
+++ index.js
@@ -1,41 +1,62 @@
-// Add JavaScript below
-
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-
+// Initialize here and run showSlide() to give your lightbox a default state.
+/*jslint white:true*/
+/*jslint browser:true */
+/*global document: false */
+/*jslint node:true */
+/* global require */
+/*jslint plusplus: true */
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlide(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// You are providing the functionality for your clickable content, which is 
+// your previews, dots, controls and the close button.
+
+function openLightbox() {
+    "use strict";
+  document.getElementById('Lightbox').style.display = 'block';
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function closeLightbox() {
+    "use strict";
+  document.getElementById('Lightbox').style.display = 'none';
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+// Note that you are assigning new values here to our slidIndex.
+
+function changeSlide(n) {
+    "use strict";
+  showSlide(slideIndex === n);
+}
+
+function toSlide(n) {
+    "use strict";
+  showSlide(slideIndex === n);
+}
+
+// This is your logic for the light box. It will decide which slide to show 
+// and which dot is active.
+
+function showSlide(n) {
+    "use strict";
+  var slides = document.getElementsByClassName('slide'), modalPreviews = document.getElementsByClassName('modal-preview'), i;
+
+  if (n > slides.length) {
+    slideIndex = 1;	
+  }
+  
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+
+  for( i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  
+  for( i = 0; i < modalPreviews.length; i++) {
+    modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  
+  slides[slideIndex - 1].style.display = 'block';
+  modalPreviews[slideIndex - 1].className += ' active';
 }
